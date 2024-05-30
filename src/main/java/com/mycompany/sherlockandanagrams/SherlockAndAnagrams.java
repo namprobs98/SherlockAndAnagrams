0cfc10fe55ea38b080d68690cf95a5070a96e9dd
Slimy Nightmare <namprobs98@gmail.com>
2024-05-30 09:37:45 +0700

    done

---
+++ src/main/java/com/mycompany/sherlockandanagrams/SherlockAndAnagrams.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sherlockandanagrams;
+
+/**
+ *
+ * @author Nhat Anh
+ */
+public class SherlockAndAnagrams {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
